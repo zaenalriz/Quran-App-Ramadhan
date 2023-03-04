37fb5572be3c807bb281f3f7b5375270768f30ce
Muhammad Zaenal <muhammadzaenal855@gmail.com>
2023-03-04 18:18:10 +0700

    commit pertama +halaman landing page

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+
+<template>
+
+  <div class="flex  justify-center w-100 h-[100vh]">
+  <div class="bg-[#DCE2E7] w-[450px] text-center  px-[50px]">
+    <div class="flex flex-col justify-center">
+        <h4 class="text-[#682CBB] font-bold text-[35px] pt-[15px]">Quran App</h4>
+        <p class="text-[#B5A8C8] text-[20px] mb-4">Read Quran <br>and listen everyday</p>
+    </div>
+    <div class="relative">
+<div class="flex justify-center">
+    <img src="./../assets/icons/landing.svg" class="h-[90%] w-[90%]"  alt="">  
+</div>
+<div  class="flex justify-center">
+    <button class="bg-[#EF9A2E] py-2 px-5 text-white absolute border rounded-3xl border-none bottom-[-15px]">Get Started</button>
+</div>
+    </div>
+    
+</div>
+</div>
+</template>
