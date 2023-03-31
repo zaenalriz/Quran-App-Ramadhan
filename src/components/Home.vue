f6c202945514210b030e8dd84cfc335ad06fa7bc
Muhammad Zaenal <muhammadzaenal855@gmail.com>
2023-03-31 22:46:25 +0700

    bug fix responsive landing page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,15 @@
 
 <template>
 
-  <div class="flex  justify-center w-100 h-[100vh]">
-  <div class="bg-[#DCE2E7] w-[450px] text-center  px-[50px]">
+  <div class="flex px-0 justify-center w-100 h-[100vh]">
+  <div class="bg-[#DCE2E7] w-[450px] text-center  px-[15px] sm:px[15px] lg:px=[50px]">
     <div class="flex flex-col justify-center">
-        <h4 class="text-[#682CBB] font-bold text-[35px] pt-[15px]">Quran App</h4>
+        <h4 class="text-[#682CBB] font-bold text-[35px] sm:pt-[5px] lg:pt-[15px]">Quran App</h4>
         <p class="text-[#B5A8C8] text-[20px] mb-4">Read Quran <br>and listen everyday</p>
     </div>
     <div class="relative">
 <div class="flex justify-center">
-    <img src="./../assets/icons/landing.svg" class="h-[90%] w-[90%]"  alt="">  
+    <img src="./../assets/icons/landing.svg" class="h-[100%] sm:h-[100%] lg:h-[450px] w-[100%]"  alt="">  
 </div>
 <div  class="flex justify-center">
     <button class="bg-[#EF9A2E] py-2 px-5 text-white absolute border rounded-3xl border-none bottom-[-15px]">Get Started</button>
