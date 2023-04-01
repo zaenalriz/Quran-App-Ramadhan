3a51791139e406aaee5a2efa76c204854ccffc85
Muhammad Zaenal <muhammadzaenal855@gmail.com>
2023-04-01 11:57:21 +0700

    +halaman list surah

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
+  
+  <div class="flex px-0 justify-center w-100 h-[100vh]">
+  <div class="bg-[#DCE2E7] w-[450px]   px-[15px] sm:px[15px] lg:px=[50px]">
   <RouterView />
+  </div>
+</div>
 </template>
---
+++ src/views/ListSurah.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pt-[50px]">
+<div class="flex items-center">
+  <img src="./../assets/icons/hamburger.svg" alt="hamburger">
+ <h4 class="text-[#682CBB] font-bold text-[20px] mx-5">Quran App</h4>
+</div>
+</div>
+<p class="text-[#B5A8C8] text-[20px] mt-3 font-bold">Welcome Brother</p>
+<div class="flex justify-center mt-4">
+  <img src="./../assets/icons/bg-list-surah.svg" alt="hamburger">
+
+</div> 
+
+</template>
+
+
