fa86b4cde502303266e0551a408d486b1a3a6b1e
Muhammad Zaenal <muhammadzaenal855@gmail.com>
2023-04-03 06:44:52 +0700

    fix bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,8 @@ import Home from '../components/Home.vue';
     <div class="flex px-0 justify-center w-100 h-[100vh]">
   <div class="bg-white w-[450px]   px-[15px] sm:px[15px] lg:px=[50px]">
     <Home />
+ </div>
+
+ </div>
  
 </template>
